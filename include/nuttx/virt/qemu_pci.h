ce21b77cf0be6d05cadf9fb48fec5eaeb5a76029
raiden00pl <raiden00@railab.me>
2024-01-20 13:40:51 +0100

    drivers/qemu_pci_test: cosmetics

--- include/nuttx/virt/qemu_pci.h
+++ include/nuttx/virt/qemu_pci.h
@@ -42,7 +42,7 @@ extern "C"
 #endif
 
 #ifdef CONFIG_VIRT_QEMU_PCI_TEST
-extern struct pcie_dev_type_s pcie_type_qemu_pci_test;
+extern struct pcie_dev_type_s g_pcie_type_qemu_pci_test;
 #endif /* CONFIG_VIRT_QEMU_PCI_TEST */
 
 #undef EXTERN
